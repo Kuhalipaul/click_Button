3d81280cf8cce83bf61b93165ba59bab750fc4df
Kuhali Paul <116445608+Kuhalipaul@users.noreply.github.com>
2023-10-16 14:23:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//First Like Button
+var btnvar1=document.getElementById("btnh1");
+
+function Toggle1(){
+    if(btnvar1.style.color=="red"){
+        btnvar1.style.color="grey"
+    }else{
+        btnvar1.style.color="red";
+    }
+}
+//Seacond Like Button
+var btnvar2=document.getElementById("btnh2");
+
+function Toggle2(){
+    if(btnvar2.style.color=="purple"){
+        btnvar2.style.color="grey"
+    }else{
+        btnvar2.style.color="purple";
+    }
+}
+//third Like Button
+var btnvar3=document.getElementById("btnh3");
+
+function Toggle3(){
+    if(btnvar3.style.color=="deeppink"){
+        btnvar3.style.color="grey"
+    }else{
+        btnvar3.style.color="deeppink";
+    }
+}
